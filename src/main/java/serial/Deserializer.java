8aea7b2f579552217d199dfac4203f23e13418a4
mrtea9 <ionmeriacri111@gmail.com>
2024-12-10 14:06:02 +0200

    parse correlation ID

--- src/main/java/serial/Deserializer.java
+++ src/main/java/serial/Deserializer.java
@@ -5,8 +5,6 @@ import util.TrackedInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 
 public class Deserializer {
 
@@ -22,6 +20,7 @@ public class Deserializer {
         final var dataInputStream = new DataInputStream(inputStream);
         //ByteBuffer wrap = ByteBuffer.wrap(test);
         System.out.println("message size " + dataInputStream.readInt());
+        System.out.println("api_key " + dataInputStream.readInt());
 
 
         final StringBuilder builder = new StringBuilder();
