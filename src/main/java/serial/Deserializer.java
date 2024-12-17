46d2d05a9a4c8f302359b93d235beb9fe8ab8f9e
mrtea9 <ionmeriacri111@gmail.com>
2024-12-17 10:50:16 +0200

    describeTopic

--- src/main/java/serial/Deserializer.java
+++ src/main/java/serial/Deserializer.java
@@ -29,7 +29,7 @@ public class Deserializer {
 
         parseBody();
 
-        final var remaining = new byte[messageSize - 25];
+        final var remaining = new byte[messageSize - 42];
         inputStream.readFully(remaining);
 
         return new KValue(header);
