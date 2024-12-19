456a33a697aeea19acae46c676b766f7a1c65621
mrtea9 <ionmeriacri111@gmail.com>
2024-12-19 11:47:52 +0200

    single partition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,7 +26,7 @@ public class Main {
 
             System.out.println(args[0]);
 
-            path = Paths.get("/tmp/kraft-combined-logs/__cluster_metadata-0/00000000000000000000.log");
+            path = Paths.get(args[0]);
 
             if (Files.exists(path)) KafkaLoader.load(path, storage);
         }
