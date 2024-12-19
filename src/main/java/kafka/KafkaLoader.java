6be9436b19a37dbe0a64a749681f2e4f63d14563
mrtea9 <ionmeriacri111@gmail.com>
2024-12-19 14:06:51 +0200

    single partition

--- src/main/java/kafka/KafkaLoader.java
+++ src/main/java/kafka/KafkaLoader.java
@@ -24,7 +24,6 @@ public class KafkaLoader {
         String serverContents = new String(serverProperties);
 
         final var logDirs = "log.dirs=";
-
         final var directoryIndex = serverContents.indexOf(logDirs);
         final var directoryPath = serverContents.substring(directoryIndex + logDirs.length());
 
